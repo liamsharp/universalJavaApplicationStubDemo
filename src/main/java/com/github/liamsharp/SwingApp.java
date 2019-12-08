b3e009e8c0906d98fa05c66f5f8d212f10d8d50b
Tobias Fischer <tobias.carl.fischer@gmail.com>
2019-12-08 13:51:32 +0100

    refactoring, indents,

--- src/main/java/com/github/liamsharp/SwingApp.java
+++ src/main/java/com/github/liamsharp/SwingApp.java
@@ -13,52 +13,44 @@ import javax.swing.SwingUtilities;
 import com.apple.eawt.Application;
 
 
-@SuppressWarnings("restriction")
-public class SwingApp
-{
-    private static final int MB = 1024 * 1024;
+public class SwingApp {
+	private static final int MB = 1024 * 1024;
 
-    private static void createAndShowGUI(
-        final List<String> args)
-    {
-        final JFrame frame = new JFrame(System.getProperty("java.version"));
+	private static void createAndShowGUI(final List<String> args) {
+		final JFrame frame = new JFrame(System.getProperty("java.version"));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final long maxMemory = Runtime.getRuntime().maxMemory() / MB;
 
-        final long maxMemory = Runtime.getRuntime().maxMemory() / MB;
-
-        String[] cp = System.getProperty("java.class.path").split(":");
-        String classpath = "";
-        for (String s : cp) {
-          classpath += s + "<br>";
-        }
+		String[] cp = System.getProperty("java.class.path").split(":");
+		String classpath = "";
+		for (String s : cp) {
+			classpath += s + "<br>";
+		}
 
 
-        JLabel label = new JLabel(
-                "<html>" +
-                "Hello!<br>" +
-                "args: " + args + "<br>" +
-                "maxMemory: " + maxMemory + "M<br>" +
-                "classpath: " + classpath +
-                "</html>");
-        frame.getContentPane().add(label);
-        frame.setMinimumSize(new Dimension(400, 300));
+		JLabel label = new JLabel(
+				"<html>" +
+						"Hello!<br/>" +
+						"args: " + args + "<br/>" +
+						"maxMemory: " + maxMemory + "M<br/>" +
+						"classpath: " + classpath +
+				"</html>");
+		frame.getContentPane().add(label);
+		frame.setMinimumSize(new Dimension(400, 300));
 
-        frame.pack();
-        frame.setVisible(true);
-    }
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-    private static void handleFileOpen(
-        final List<File> files)
-    {
-       JOptionPane.showMessageDialog(null, files);
-    }
+	private static void handleFileOpen(final List<File> files) {
+		JOptionPane.showMessageDialog(null, files);
+	}
 
-    public static void main(String[] args)
-    {
-        final Application appleApp = Application.getApplication();
-        appleApp.setOpenFileHandler(e -> SwingUtilities.invokeLater(() -> handleFileOpen(e.getFiles())));
-        javax.swing.SwingUtilities.invokeLater( () -> createAndShowGUI(Arrays.asList(args)));
-    }
+	public static void main(String[] args) {
+		final Application appleApp = Application.getApplication();
+		appleApp.setOpenFileHandler(e -> SwingUtilities.invokeLater(() -> handleFileOpen(e.getFiles())));
+		javax.swing.SwingUtilities.invokeLater( () -> createAndShowGUI(Arrays.asList(args)));
+	}
 
 }
